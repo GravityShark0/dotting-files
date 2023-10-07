faff5e76f3237d8c3f897455b6ea45411d5205bf
gravitys <gravityshark@artix.localdomain>
2023-10-07 15:56:40 +0800

    yeah baby stylua

--- awesome/grav.lua
+++ awesome/grav.lua
@@ -2,35 +2,35 @@
 -- Default awesome theme --
 ---------------------------
 
-local theme_assets = require("beautiful.theme_assets")
-local xresources = require("beautiful.xresources")
+local theme_assets = require('beautiful.theme_assets')
+local xresources = require('beautiful.xresources')
 local dpi = xresources.apply_dpi
 
-local gfs = require("gears.filesystem")
+local gfs = require('gears.filesystem')
 local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "Ubuntu Nerd Font 8"
+theme.font = 'Ubuntu Nerd Font 8'
 
-theme.bg_normal     = "#1b1a29"
-theme.bg_focus      = "#302d41"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+theme.bg_normal = '#1b1a29'
+theme.bg_focus = '#302d41'
+theme.bg_urgent = '#ff0000'
+theme.bg_minimize = '#444444'
+theme.bg_systray = theme.bg_normal
 
-theme.fg_cosmic     = "#fff8e7"
-theme.fg_normal     = "#c9cbff"
-theme.fg_focus      = "#fff8e7"
-theme.fg_urgent     = "#fff8e7"
-theme.fg_minimize   = "#fff8e7"
+theme.fg_cosmic = '#fff8e7'
+theme.fg_normal = '#c9cbff'
+theme.fg_focus = '#fff8e7'
+theme.fg_urgent = '#fff8e7'
+theme.fg_minimize = '#fff8e7'
 
-theme.useless_gap   = dpi(1)
-theme.border_width  = dpi(2)
+theme.useless_gap = dpi(1)
+theme.border_width = dpi(2)
 theme.border_normal = theme.bg_normal
-theme.border_focus  = "#ddb7f2"
-theme.border_scratch= "#cda869"
-theme.border_marked = "#91231c"
+theme.border_focus = '#ddb7f2'
+theme.border_scratch = '#cda869'
+theme.border_marked = '#91231c'
 
 -- There are other variable sets
 -- overriding the default one when
@@ -47,12 +47,10 @@ theme.border_marked = "#91231c"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
+theme.taglist_squares_sel =
+	theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel =
+	theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
 
 -- Variables set for theming notifications:
 -- notification_font
@@ -63,9 +61,9 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path .. 'default/submenu.png'
 theme.menu_height = dpi(15)
-theme.menu_width  = dpi(100)
+theme.menu_width = dpi(100)
 
 -- You can add as many variables as
 -- you wish and access them by using
@@ -102,27 +100,26 @@ theme.menu_width  = dpi(100)
 -- theme.wallpaper = themes_path.."default/background.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = themes_path .. 'default/layouts/fairhw.png'
+theme.layout_fairv = themes_path .. 'default/layouts/fairvw.png'
+theme.layout_floating = themes_path .. 'default/layouts/floatingw.png'
+theme.layout_magnifier = themes_path .. 'default/layouts/magnifierw.png'
+theme.layout_max = themes_path .. 'default/layouts/maxw.png'
+theme.layout_fullscreen = themes_path .. 'default/layouts/fullscreenw.png'
+theme.layout_tilebottom = themes_path .. 'default/layouts/tilebottomw.png'
+theme.layout_tileleft = themes_path .. 'default/layouts/tileleftw.png'
+theme.layout_tile = themes_path .. 'default/layouts/tilew.png'
+theme.layout_tiletop = themes_path .. 'default/layouts/tiletopw.png'
+theme.layout_spiral = themes_path .. 'default/layouts/spiralw.png'
+theme.layout_dwindle = themes_path .. 'default/layouts/dwindlew.png'
+theme.layout_cornernw = themes_path .. 'default/layouts/cornernww.png'
+theme.layout_cornerne = themes_path .. 'default/layouts/cornernew.png'
+theme.layout_cornersw = themes_path .. 'default/layouts/cornersww.png'
+theme.layout_cornerse = themes_path .. 'default/layouts/cornersew.png'
 
 -- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, theme.bg_focus, theme.fg_focus
-)
+theme.awesome_icon =
+	theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
